9a83472edf68a390ae66e2a32bf490318d7c6322
cloudqubes <cloud.qubes@gmail.com>
2024-11-09 09:41:55 +0530

    Removed rangy.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,3 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "turndown" # @7.2.0
 pin "tippy.js" # @6.3.7
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "rangy" # @1.3.2
